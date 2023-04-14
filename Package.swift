1047162a9a141dfe06feebff3e8ad924583fb3b9
Vova Skripnichenko <52348214+VVSKR@users.noreply.github.com>
2023-04-14 10:30:13 +0300

    Fixed package file

--- Package.swift
+++ Package.swift
@@ -1,7 +1,22 @@
 // swift-tools-version:5.5
-
 import PackageDescription
 
 let package = Package(
-    name: "LayoutKit"
+    name: "LayoutKit",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "LayoutKit",
+            targets: ["LayoutKit"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "LayoutKit",
+            path: "Sources"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
